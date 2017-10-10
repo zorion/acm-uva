55b060d0ef07830b602db31bf6ec816b6d2ce08a
Zorion <zorionk@gmail.com>
2017-10-10 23:52:21 +0200

    Problem 100. From RE to TLE.

--- vol001/100_python/3n.py
+++ vol001/100_python/3n.py
@@ -1,3 +1,4 @@
+from sys import stdin
 
 def alg(n):
     i = 1
@@ -13,19 +14,19 @@ def alg(n):
 def get_max_cycles(i, j):
     if j < i:
         i, j = j, i
-    current_max = alg(j)
+    current_max = alg(i)
     idx = i
     while idx < j:
+        idx += 1
         cycles = alg(idx)
         if cycles > current_max:
             current_max = cycles
-        idx += 1
     return current_max
 
 
 def read_input():
     try:
-        data_in = input("")
+        data_in = stdin.readline().split("\n")[0]
         return data_in
     except EOFError:
         return None
@@ -34,12 +35,13 @@ def read_input():
 def data_out(i, j, max_cycles):
     print(i, j, max_cycles)
 
+
 def main():
     while True:
         data_in = read_input()
         if not data_in:
             break
-        i, j = map(int, data_in.split(" "))
+        i, j = map(int, data_in.split()[:2])
         max_cycles = get_max_cycles(i, j)
         data_out(i, j, max_cycles)
 
