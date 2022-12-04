096b63e66a491613c631592a229307ea563ed903
ZK Arrizabalaga <zarrizabalaga@g-n.com>
2022-12-04 11:41:08 +0100

    AoC 2022: Add day04.

---
+++ advent-of-code/2022/day04/prog1.hs
@@ -0,0 +1,34 @@
+main = do
+    interact prob1
+    putStrLn ""
+
+prob1 :: String -> String
+prob1 = show.sum.(map findRedundants).(map reorder).getInput
+
+getInput :: String -> [(Int, Int, Int, Int)]
+getInput = (map split4).lines
+
+split4 :: String -> (Int, Int, Int, Int)
+split4 s = (read a, read b, read c, read d)
+    where
+        (a, b) = split2 '-' x
+        (c, d) = split2 '-' y
+        (x, y) = split2 ',' s
+
+split2 :: Char -> String -> (String, String)
+split2 d s = (a, tail b)
+    where (a, b) = break (==d) s
+
+reorder :: (Int, Int, Int, Int) -> (Int, Int, Int, Int)
+reorder (w,x,y,z) = (a,b,c,d)
+    where
+        minmax p q = (min p q, max p q)
+        (a, b) = minmax w x
+        (c, d) = minmax y z
+
+findRedundants :: (Int, Int, Int, Int) -> Int
+findRedundants (w, x, y, z)
+    | and [(w <= y), (x >= z)] = 1
+    | and [(y <= w), (z >= x)] = 1
+    | otherwise = 0
+
