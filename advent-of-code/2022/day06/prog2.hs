7a7bf9915387205e620cfaefc51edc4ae5473080
ZK Arrizabalaga <zarrizabalaga@g-n.com>
2022-12-06 15:05:52 +0100

    AoC 2022: Simplify the part2 of day06.

--- advent-of-code/2022/day06/prog2.hs
+++ advent-of-code/2022/day06/prog2.hs
@@ -1,30 +1,26 @@
+main :: IO ()
 main = do
     interact prob1
     putStrLn ""
 
+
 prob1 :: String -> String
 prob1 = show.(map (getDiffChar 14)).lines
 
+
 getDiffChar :: Int -> String -> Int
-getDiffChar n xs = aux n xs []
-    where aux 0 xs' ys = gdAux n xs' ys
-          aux m (x:xs') ys = aux (m-1) xs' (ys++[x])
-
-gdAux4 :: Char -> Char -> Char -> Char -> Int -> String -> Int
-gdAux4 a b c d n [] = n
-gdAux4 a b c d n (x:xs)
-    | not (repeated [a,b,c,d]) = n
-    | otherwise = gdAux4 b c d x (n+1) xs
-
-gdAux :: Int -> String -> String -> Int
-gdaux n [] (y:ys) = n
-gdAux n (x:xs) (y:ys)
-    | not (repeated (y:ys)) = n
-    | otherwise = gdAux (n+1) xs (ys ++ [x])
+getDiffChar n xs = getAllDiffPosition n (drop n xs) (take n xs)
 
 
+getAllDiffPosition :: Int -> String -> String -> Int
+getAllDiffPosition n [] (y:ys) = n
+getAllDiffPosition n (x:xs) (y:ys)
+    | not (hasRepeated (y:ys)) = n
+    | otherwise = getAllDiffPosition (n+1) xs (ys ++ [x])
 
-repeated [] = False
-repeated (x:xs)
+
+hasRepeated :: Eq a => [a] -> Bool
+hasRepeated [] = False
+hasRepeated (x:xs)
     | elem x xs = True
-    | otherwise = repeated xs
+    | otherwise = hasRepeated xs
