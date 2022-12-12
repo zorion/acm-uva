8b5eeaf7016f204e5e563846aa7cc2b3b420e9a2
ZK Arrizabalaga <zarrizabalaga@g-n.com>
2022-12-12 09:50:23 +0100

    AoC22: day11 without cheating.
    We get the "reducer" from the input.
    
    With the reducer we don't need Items to be Integer (arbitrarily long
    integers) so Int is fine.

--- advent-of-code/2022/day11/prog2.hs
+++ advent-of-code/2022/day11/prog2.hs
@@ -7,12 +7,11 @@ main = do
 
 prob1 :: String -> String
 prob1 = show.evaluate.getStatus.(simulate 10000).getInput
--- myin: 15333249714
 
 type State = (Inspections, Items)
 type Inspections = DM.Map Monkey Int
 type Items = DM.Map Monkey [Item]
-type Item = Integer
+type Item = Int  -- with the "`mod` 2*3*5*7*11*13*17*19*23" hack we don't need Integer
 
 type Monkey = Int
 type Rules = DM.Map Monkey Rule
@@ -23,8 +22,8 @@ fakeRule :: Rule
 fakeRule = ((+0), (==0), 0,0)
 
 
-getStatus :: (Rules, State) -> State
-getStatus (r, s) = s
+getStatus :: (Int, Rules, State) -> State
+getStatus (_, _, s) = s
 getInspections :: State -> Inspections
 getInspections (ins, its) = ins
 
@@ -33,16 +32,16 @@ evaluate (inspections, _) = product (take 2 bestValues)
     where bestValues = qs allValues  --quicksort
           allValues = DM.elems inspections
 
-simulate :: Int -> (Rules, State) -> (Rules, State)
-simulate 0 (rules, state) = (rules, state)
-simulate n (rules, state) = simulate (n-1) (rules, simulateRound rules state)
+simulate :: Int -> (Int, Rules, State) -> (Int, Rules, State)
+simulate 0 (reducer, rules, state) = (reducer, rules, state)
+simulate n (reducer, rules, state) = simulate (n-1) (reducer, rules, simulateRound reducer rules state)
 
-simulateRound :: Rules -> State -> State
-simulateRound rules state = foldl (simulateTurn rules) state (DM.keys rules)
+simulateRound :: Int -> Rules -> State -> State
+simulateRound reducer rules state = foldl (simulateTurn reducer rules) state (DM.keys rules)
 
 -- For each item in the Monkey's Items apply the Rule, add Inspections and throw it
-simulateTurn :: Rules -> State -> Monkey -> State
-simulateTurn rules state monkey = foldl (simulateRule ruleMonkey) statePartial oldItemsMonkey
+simulateTurn :: Int -> Rules -> State -> Monkey -> State
+simulateTurn reducer rules state monkey = foldl (simulateRule reducer ruleMonkey) statePartial oldItemsMonkey
     where (oldInspections, oldItems) = state
           oldInspMonkey = DM.findWithDefault 0 monkey oldInspections
           newInspections = DM.insert monkey (oldInspMonkey + (length oldItemsMonkey)) oldInspections
@@ -51,15 +50,16 @@ simulateTurn rules state monkey = foldl (simulateRule ruleMonkey) statePartial o
           itemsPartial = DM.insert monkey [] oldItems
           ruleMonkey = DM.findWithDefault fakeRule monkey rules
 
-simulateRule :: Rule -> State -> Item -> State
-simulateRule rule state item = (inspections, newItems)
+simulateRule :: Int -> Rule -> State -> Item -> State
+simulateRule reducer rule state item = (inspections, newItems)
     where (op, t, mTrue, mFalse) = rule
           (inspections, oldItems) = state
-          newItem = mod (op item) (23*19*13*17*5*7*2*3*11)  -- no longer divided by three
+          newItem = (op item) `mod` reducer  -- no longer divided by three
           newMonkey = if t newItem then mTrue else mFalse
           oldMonkeyItems = DM.findWithDefault [] newMonkey oldItems
           newItems = DM.insert newMonkey (oldMonkeyItems ++ [newItem]) oldItems
 
+-- reducer = 23*19*13*17*5*7*2*3*11
 
 ----------------------------------------
 -- WORK ON INPUTS
@@ -72,27 +72,28 @@ simulateRule rule state item = (inspections, newItems)
 --    If false: throw to monkey Int
 --(ENDLINE)
 
-getInput :: String -> (Rules, State)
-getInput input = getPartialInput (DM.empty, (DM.empty, DM.empty)) 0 (lines input)
+getInput :: String -> (Int, Rules, State)
+getInput input = getPartialInput (1, DM.empty, (DM.empty, DM.empty)) 0 (lines input)
 
-getPartialInput :: (Rules, State) -> Monkey -> [String] -> (Rules, State)
+getPartialInput :: (Int, Rules, State) -> Monkey -> [String] -> (Int, Rules, State)
 getPartialInput status _ [] = status
 getPartialInput status n ("":ys) = getPartialInput status n ys
 getPartialInput status _ (('M':'o':'n':'k':'e':'y':' ':xs):ys) = getPartialInput status (read(take 1 xs)) ys
 getPartialInput status n ((' ':' ':'S':'t':'a':'r':'t':'i':'n':'g':' ':'i':'t':'e':'m':'s':':':' ':xs):ys) = getPartialInput newStatus n ys
-    where newStatus = (rules, newStat)
-          (rules, oldStat) = status
+    where newStatus = (moder, rules, newStat)
+          (moder, rules, oldStat) = status
           newStat = (inspections, newItems)
           (inspections, oldItems) = oldStat
           newItems = DM.insert n (parseItems xs) oldItems
 getPartialInput status n (ops:test:monkeyTrue:monkeyFalse:ys3) = getPartialInput newStatus n ys3
-    where newStatus = (newRules, stat)
-          (oldRules, stat) = status
+    where newStatus = (newModer, newRules, stat)
+          (moder, oldRules, stat) = status
           operation = parseOperation(ops)
-          testOp = parseTest(test)
+          (newM, testOp) = parseTest(test)
           mTrue = parseMonkey(monkeyTrue)
           mFalse = parseMonkey(monkeyFalse)
           newRules = DM.insert n (operation, testOp, mTrue, mFalse) oldRules
+          newModer = moder * newM
 
 
 parseItems :: String -> [Item]
@@ -114,8 +115,8 @@ parseOperation xs
               drop2restOp = drop 2 restOp
 
 --  Test: divisible by Int
-parseTest :: String -> (Item -> Bool)
-parseTest xs = \x -> 0 == (mod x numDiv)
+parseTest :: String -> (Int, Item -> Bool)
+parseTest xs = (numDiv, \x -> 0 == (mod x numDiv))
     where numDiv= read (drop (length "  Test: divisible by ") xs)
 
 --    If true: throw to monkey Int
