575823dedfee1c2d991c4f6884beb30fe1f59d6e
ZK Arrizabalaga <zarrizabalaga@g-n.com>
2023-12-01 09:58:40 +0100

    AoC-23-day01: Autoformat.

--- advent-of-code/2023/day01/prog1.hs
+++ advent-of-code/2023/day01/prog1.hs
@@ -1,11 +1,10 @@
-
 main :: IO ()
 main = do
-    interact prob1
-    putStrLn ""
+  interact prob1
+  putStrLn ""
 
 prob1 :: String -> String
-prob1 = show.solve1.readIn
+prob1 = show . solve1 . readIn
 
 -- SUM
 solve1 :: [Int] -> (Int, [Int])
@@ -19,19 +18,22 @@ readNum :: String -> Int
 readNum x = (10 * readFirst x) + readLast x
 
 readFirst :: String -> Int
-readFirst (x:xs) = if and[x>='0', x<='9']
-                   then read [x]
-                   else readFirst xs
+readFirst (x : xs) =
+  if and [x >= '0', x <= '9']
+    then read [x]
+    else readFirst xs
+
 readLast :: String -> Int
 readLast x = case readLastMaybe x of
-    Just num -> num
-    Nothing -> -1
+  Just num -> num
+  Nothing -> -1
 
 readLastMaybe :: String -> Maybe Int
 readLastMaybe [] = Nothing
-readLastMaybe [x] = if and[x>='0', x<='9']
+readLastMaybe [x] =
+  if and [x >= '0', x <= '9']
     then Just (read [x])
     else Nothing
-readLastMaybe (x:xs) = case readLastMaybe xs of
-    Just num -> Just num
-    Nothing -> readLastMaybe [x]
+readLastMaybe (x : xs) = case readLastMaybe xs of
+  Just num -> Just num
+  Nothing -> readLastMaybe [x]
--- advent-of-code/2023/day01/prog2.hs
+++ advent-of-code/2023/day01/prog2.hs
@@ -1,11 +1,10 @@
-
 main :: IO ()
 main = do
-    interact prob1
-    putStrLn ""
+  interact prob1
+  putStrLn ""
 
 prob1 :: String -> String
-prob1 = show.solve1.readIn
+prob1 = show . solve1 . readIn
 
 -- SUM
 solve1 :: [Int] -> (Int, [Int])
@@ -16,35 +15,35 @@ readIn :: String -> [Int]
 readIn x = map readNum (lines x)
 
 readNum :: String -> Int
-readNum x = (10 * head tokens) + tokens!!(-1 + length tokens) where
+readNum x = (10 * head tokens) + tokens !! (-1 + length tokens)
+  where
     tokens = getTokens x
 
 getTokens :: String -> [Int]
 getTokens [] = []
 getTokens xs = case getFirstToken xs of
-    Just num -> num : getTokens(tail xs)
-    Nothing -> getTokens(tail xs)
+  Just num -> num : getTokens (tail xs)
+  Nothing -> getTokens (tail xs)
 
 getFirstToken :: String -> Maybe Int
 getFirstToken x
-    | take 1 x == "0" = Just 0
-    | take 1 x == "1" = Just 1
-    | take 1 x == "2" = Just 2
-    | take 1 x == "3" = Just 3
-    | take 1 x == "4" = Just 4
-    | take 1 x == "5" = Just 5
-    | take 1 x == "6" = Just 6
-    | take 1 x == "7" = Just 7
-    | take 1 x == "8" = Just 8
-    | take 1 x == "9" = Just 9
-    | take 3 x == "one" = Just 1
-    | take 3 x == "two" = Just 2
-    | take 5 x == "three" = Just 3
-    | take 4 x == "four" = Just 4
-    | take 4 x == "five" = Just 5
-    | take 3 x == "six" = Just 6
-    | take 5 x == "seven" = Just 7
-    | take 5 x == "eight" = Just 8
-    | take 4 x == "nine" = Just 9
-    | otherwise = Nothing
-    
+  | take 1 x == "0" = Just 0
+  | take 1 x == "1" = Just 1
+  | take 1 x == "2" = Just 2
+  | take 1 x == "3" = Just 3
+  | take 1 x == "4" = Just 4
+  | take 1 x == "5" = Just 5
+  | take 1 x == "6" = Just 6
+  | take 1 x == "7" = Just 7
+  | take 1 x == "8" = Just 8
+  | take 1 x == "9" = Just 9
+  | take 3 x == "one" = Just 1
+  | take 3 x == "two" = Just 2
+  | take 5 x == "three" = Just 3
+  | take 4 x == "four" = Just 4
+  | take 4 x == "five" = Just 5
+  | take 3 x == "six" = Just 6
+  | take 5 x == "seven" = Just 7
+  | take 5 x == "eight" = Just 8
+  | take 4 x == "nine" = Just 9
+  | otherwise = Nothing
