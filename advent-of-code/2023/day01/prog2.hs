8f73e6f96439611797cb9b43390dc49262c39142
ZK Arrizabalaga <zarrizabalaga@g-n.com>
2023-12-01 09:23:41 +0100

    Prepare a explicit pattern checker.

--- advent-of-code/2023/day01/prog2.hs
+++ advent-of-code/2023/day01/prog2.hs
@@ -21,6 +21,21 @@ readNum x = (10 * head tokens) + tokens!!(-1 + length tokens) where
 
 getTokens :: String -> [Int]
 getTokens [] = []
-getTokens (x:xs) = if (x <= '9') && (x >= '0')
-    then read [x] : getTokens xs
-    else getTokens xs
+getTokens xs = case getFirstToken xs of
+    Just num -> num : getTokens(tail xs)
+    Nothing -> getTokens(tail xs)
+
+getFirstToken :: String -> Maybe Int
+getFirstToken x
+    | take 1 x == "0" = Just 0
+    | take 1 x == "1" = Just 1
+    | take 1 x == "2" = Just 2
+    | take 1 x == "3" = Just 3
+    | take 1 x == "4" = Just 4
+    | take 1 x == "5" = Just 5
+    | take 1 x == "6" = Just 6
+    | take 1 x == "7" = Just 7
+    | take 1 x == "8" = Just 8
+    | take 1 x == "9" = Just 9
+    | otherwise = Nothing
+    
