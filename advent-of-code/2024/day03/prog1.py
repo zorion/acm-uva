343e98eddf08567983b79c5fc33ac6ba65c29ea6
ZK Arrizabalaga <zarrizabalaga@g-n.com>
2024-12-03 17:04:59 +0100

    Day03: Fix potential muls between lines.

--- advent-of-code/2024/day03/prog1.py
+++ advent-of-code/2024/day03/prog1.py
@@ -9,11 +9,11 @@ def main():
 
 def execute_instructions(instructions: list[str]) -> list[int]:
     result = []
-    for instruct in instructions:
-        matches = re.findall(r"mul\([0-9]{1,3},[0-9]{1,3}\)", instruct)
-        for match in matches:
-            match = match[4:-1]
-            result.append(int(match.split(",")[0]) * int(match.split(",")[1]))
+    instruct = "".join(instructions)
+    matches = re.findall(r"mul\([0-9]{1,3},[0-9]{1,3}\)", instruct)
+    for match in matches:
+        match = match[4:-1]
+        result.append(int(match.split(",")[0]) * int(match.split(",")[1]))
     return result
 
 
