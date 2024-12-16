616f31c2bb7c3fe804cf8535f84d44b2a244355b
ZK Arrizabalaga <zarrizabalaga@g-n.com>
2024-12-16 11:50:20 +0100

    Day15: Part1.

--- advent-of-code/2024/day15/part1.py
+++ advent-of-code/2024/day15/part1.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-type Maze = list[str]
+type Maze = list[list[str]]
 
 
 def main():
@@ -10,9 +10,49 @@ def main():
 
 
 def solve_puzzle(mapin: Maze, instructions: str) -> Maze:
-    print(mapin)
-    print(instructions)
+    for instr in instructions:
+        apply_instr(mapin, instr)
     return mapin
+
+
+def print_maze(mapin: Maze) -> None:
+    for line in mapin:
+        print("".join(line))
+
+
+def apply_instr(mapin: Maze, instr: str) -> None:
+    rx, ry = find_robot(mapin)
+    if instr == "<":
+        move = (-1, 0)
+    elif instr == ">":
+        move = (1, 0)
+    elif instr == "^":
+        move = (0, -1)
+    elif instr == "v":
+        move = (0, 1)
+    else:
+        raise ValueError(f"Invalid instruction: {instr}")
+    px, py = rx, ry
+    stack = []
+    while mapin[py][px] not in ["#", "."]:
+        stack.append((px, py))
+        px, py = px + move[0], py + move[1]
+    if mapin[py][px] == "#":
+        # Do nothing
+        return
+    assert mapin[py][px] == "."
+    while stack:
+        x, y = stack.pop()
+        mapin[py][px] = mapin[y][x]
+        px, py = x, y
+    mapin[ry][rx] = "."
+
+
+def find_robot(mapin: Maze) -> tuple[int, int]:
+    for j, line in enumerate(mapin):
+        for i, c in enumerate(line):
+            if c == "@":
+                return i, j
 
 
 def read_puzzle() -> tuple[Maze, str]:
@@ -24,7 +64,7 @@ def read_puzzle() -> tuple[Maze, str]:
                 break
         except Exception:
             break
-        maze.append(line)
+        maze.append([c for c in line])
     instructions = ""
     while True:
         try:
